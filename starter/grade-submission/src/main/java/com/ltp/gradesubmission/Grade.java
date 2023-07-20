a8f45993c002debdc6afd2557d1b9233065b1597
sanchi285 <bismoy.sanchi@gmail.com>
2023-07-21 00:30:12 +0600

    Model Attribute has been Added

---
+++ starter/grade-submission/src/main/java/com/ltp/gradesubmission/Grade.java
@@ -0,0 +1,42 @@
+package com.ltp.gradesubmission;
+
+public class Grade {
+
+    private String name;
+    private String subject;
+    private String score;
+
+    public Grade(String name, String subject, String score) {
+        this.name = name;
+        this.subject = subject;
+        this.score = score;
+    }
+
+    public Grade(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
--- starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeConrtoller.java
+++ starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeConrtoller.java
@@ -1,12 +1,18 @@
 
-package com.lpt.gradesubmission;
+package com.ltp.gradesubmission;
 import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import  org.springframework.ui.Model;
+import org.thymeleaf.model.IModel;
 
 @Controller
 public class GradeConrtoller {
    
     @GetMapping("/hello")
-    public String sayHello(){
-        return "some htmlview";
+    public String sayHello(Model model){
+        Grade grade = new Grade("Harry","Portion","A");
+        model.addAttribute("grade",grade);
+        return "grades";
     }
 }
